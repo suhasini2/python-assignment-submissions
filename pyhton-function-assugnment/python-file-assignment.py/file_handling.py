34c76f7e5db003f8e6a953a84409b3088752bc54
suhasini2 <msuhasini003@gmail.com>
2026-02-20 08:36:34 +0530

    Submitting file assignment

---
+++ pyhton-function-assugnment/python-file-assignment.py/file_handling.py
@@ -0,0 +1,17 @@
+sum=0
+with open("python-files-assignment.py/numbers.txt","r") as file:
+    lines=file.readlines()
+
+total_numbers_lines=len(lines)
+
+for number in lines:
+    sum+=int(number)
+
+avg=sum/total_numbers_lines
+
+with open("results.log","a") as logs:
+    logs.write("File opened successfully\n")
+    logs.write(f"Read {total_numbers_lines} numbers\n")
+    logs.write(f"Sum: {sum}\n")
+    logs.write(f"Average: {avg}\n")
+    logs.write("Processing completed\n")
